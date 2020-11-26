b06e2a81200ad965b16f25c7765933f270b5c34d
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2020-11-26 23:40:41 +0000

    getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -28,7 +28,6 @@ const getUserWithEmail = function(email) {
     if (res.rows.length === 0) {
       return null
     } 
-    console.log(res.rows[0])
     return res.rows[0]
   })
 }
@@ -80,7 +79,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  let values = [guest_id, limit];
+  let sqlQuery = `SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`
+
+  return pool.query(sqlQuery, values).then(res => {
+    console.log(res.rows[0])
+    res.rows[0]
+
+  })
 }
 exports.getAllReservations = getAllReservations;
 
